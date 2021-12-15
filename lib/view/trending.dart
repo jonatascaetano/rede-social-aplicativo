a5bebfe8180a496bd98603bd27eb3d69082c07ba
Jonatas Caetano <jonatas.calves@gmail.com>
2021-12-15 13:40:56 -0300

    Main page implemented

---
+++ lib/view/trending.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Trending extends StatefulWidget {
+  const Trending({Key? key}) : super(key: key);
+
+  @override
+  _TrendingState createState() => _TrendingState();
+}
+
+class _TrendingState extends State<Trending> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Trending"));
+  }
+}
